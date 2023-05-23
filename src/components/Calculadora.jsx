49bd34ee8128a1d696ddb752fe33f4308b2edd58
VictorSilva244 <victor.silva37@outlook.com>
2023-05-22 23:00:10 -0300

    Separação de um um componente e inicio das func

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import cifrao from "../images/cifrao.png"
 import pessoa from "../images/pessoa.png"
 import { 
@@ -7,14 +8,29 @@ import {
     MainContainer,
     ButtonsContainer, 
     PorcentagemContainer,
-    PessoasContainer,
-    ResultadosContainer,
-    GorjetaContainer,
-    TotalContainer,
-    GorjetaTotalContainer
+    PessoasContainer
 } from "./Calculadora.styles"
+import { Resultados } from "./Resultados"
 
 export function Calculadora(){
+    const[conta, setConta] = useState(0)
+    const[pessoas, setPessoas] = useState(0)
+    const[porcentagem, setPorcentagem] = useState(0)
+
+
+    function handleMudarValorConta(event){
+        setConta(Number(event.target.value))
+        
+    }
+
+    function handleMudarQuantidadePessoas(event){
+        setPessoas(Number(event.target.value))
+    }
+
+    function  handleMudarPorcentagem(event, numero){
+
+    }
+
     return(
         <MainContainer>
             
@@ -24,19 +40,19 @@ export function Calculadora(){
                     <Label htmlFor="bill">Conta</Label>
                     <InputContainer>
                         <img src={cifrao} alt="" />
-                        <input type="number" id="bill" placeholder="0"/>
+                        <input type="number" id="bill" placeholder="0" onChange={handleMudarValorConta}/>
                     </InputContainer>
                 </ContaContainer>
 
                 <PorcentagemContainer>
                     <Label htmlFor="custom-tip">Selecione a Porcentagem %</Label>
                     <ButtonsContainer>
-                        <input type="button" value="5%" />
-                        <input type="button" value="10%" />
-                        <input type="button" value="15%" />
-                        <input type="button" value="25%" />
-                        <input type="button" value="50%" />
-                        <input type="number" id="custom-tip" placeholder="Outro"/>
+                        <input type="button" value="5%"  onClick={() => handleMudarPorcentagem(0,5)}/>
+                        <input type="button" value="10%" onClick={() => handleMudarPorcentagem(0,10)}/>
+                        <input type="button" value="15%" onClick={() => handleMudarPorcentagem(0,15)}/>
+                        <input type="button" value="25%" onClick={() => handleMudarPorcentagem(0,25)}/>
+                        <input type="button" value="50%" onClick={() => handleMudarPorcentagem(0,50)} />
+                        <input type="number" id="custom-tip" placeholder="Outro" onChange={handleMudarPorcentagem} />
                     </ButtonsContainer>
                 </PorcentagemContainer>
 
@@ -44,32 +60,12 @@ export function Calculadora(){
                     <Label htmlFor="people">Número de Pessoas</Label>
                         <InputContainer>
                             <img src={pessoa} alt="" />
-                            <input type="number" id="people" placeholder="0"/>
+                            <input type="number" id="people" placeholder="0"  onChange={handleMudarQuantidadePessoas}/>
                         </InputContainer>
                 </PessoasContainer>
             </form>
 
-            <ResultadosContainer>
-                <GorjetaTotalContainer>
-                    <GorjetaContainer>
-                        <p>
-                            Gorjeta <br/>
-                            <span>/ pessoa</span>
-                        </p>
-                        <p>R$ 0.00</p>
-                    </GorjetaContainer>
-
-                    <TotalContainer>
-                        <p>
-                            Total <br/>
-                            <span>/ pessoa</span>
-                        </p>
-                        <p>R$ 0.00</p>
-                    </TotalContainer>
-                </GorjetaTotalContainer>
-                <button>Limpar</button>
-
-            </ResultadosContainer>
+            <Resultados/>
         </MainContainer>
     )
 }
--- src/components/Calculadora.styles.js
+++ src/components/Calculadora.styles.js
@@ -32,6 +32,11 @@ export const InputContainer = styled.div`
     align-items:center;
     border-radius:5px;
 
+    &:has(input:hover),
+    &:has(input:focus){
+       outline: 2px solid #26c2ae
+    }
+
     input {
         border:0;
         background:transparent;
@@ -75,6 +80,11 @@ export const ButtonsContainer = styled.div`
     input[type = "button"] {
         background:#00474b;
         color:white;
+
+        &:hover{
+            background:#9fe8df;
+            cursor:pointer;
+        }
     }
 
     input#custom-tip{
@@ -83,6 +93,11 @@ export const ButtonsContainer = styled.div`
         text-align: center;
         color: #00474b;
         
+        &:hover,
+        &:focus{
+            outline: 2px solid #26c2ae
+        }
+
         &::placeholder{
             color: #547878;
         }
@@ -103,54 +118,3 @@ export const ButtonsContainer = styled.div`
 export const PorcentagemContainer = styled(ContaContainer)``
 
 export const PessoasContainer = styled(ContaContainer)``
-
-export const ResultadosContainer = styled.div`
-    background-color: #00474b;
-    border-radius:15px;
-    padding: 2rem 1.5rem;
-
-    button {
-        width:100%;
-        color: #00474b;
-        background: #26c2ae;
-        border:0;
-        font-size:1.25rem;
-        padding-block: 0.5rem;
-        text-transform:uppercase;
-        border-radius:5px;
-    }
-
-    @media (min-width: 950px){
-        display:flex;
-        flex-direction:column;
-        justify-content:space-between;
-    }
-`
-export const GorjetaContainer = styled.div`
-    display:flex;
-    margin-bottom: 1.25rem;
-    justify-content:space-between;
-    align-items:center;
-
-    p:first-child{
-        color:white;
-        line-height:1.5rem;
-
-        span {
-            color: #7f9d9f;
-            font-size: 0.75rem;
-        }
-    }
-
-    p:last-child{
-        color: #26c2ae;
-        font-size:2rem;
-        line-height:3rem;
-    }
-`
-
-export const TotalContainer = styled(GorjetaContainer)`
-    margin-bottom:2rem;
-`
-
-export const GorjetaTotalContainer = styled.div``
